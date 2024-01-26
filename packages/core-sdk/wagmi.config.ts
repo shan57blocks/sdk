87733ed71a3a9d6008416029811da3b2e2a2929a
Don <100505855+DonFungible@users.noreply.github.com>
2024-01-25 16:01:03 -0800

    test wagmi

--- packages/core-sdk/wagmi.config.ts
+++ packages/core-sdk/wagmi.config.ts
@@ -1,17 +1,37 @@
+import { Abi } from "viem";
 import { defineConfig } from "@wagmi/cli";
-import { actions, foundry } from "@wagmi/cli/plugins";
+import { actions, react } from "@wagmi/cli/plugins";
+
+import { abi as ipAccountRegistryAbi } from "../protocol-core/out/IIPAccountRegistry.sol/IIPAccountRegistry.json";
+import { abi as registrationModuleAbi } from "../protocol-core/out/RegistrationModule.sol/RegistrationModule.json";
 
 export default defineConfig({
-  out: "lib/generated.ts",
-  plugins: [
-    foundry({
-      project: "../protocol-core",
-      exclude: ["../protocol-core/lib/reference/src/interfaces/**"],
-    }),
-    actions({
-      getActionName({ contractName, type }) {
-        return `${contractName}__${type}`;
-      },
-    }),
+  out: "src/generated/generated.ts",
+  contracts: [
+    {
+      name: "IPAccountImpl",
+      abi: ipAccountRegistryAbi as Abi,
+    },
+    {
+      name: "RegistrationModule",
+      abi: registrationModuleAbi as Abi,
+    },
   ],
+  // plugins: [actions()],
+  plugins: [actions(), react()],
 });
+
+// export default defineConfig({
+//   out: "lib/generated.ts",
+//   plugins: [
+//     foundry({
+//       project: "../protocol-core",
+//       exclude: ["../protocol-core/lib/reference/src/interfaces/**"],
+//     }),
+//     actions({
+//       getActionName({ contractName, type }) {
+//         return `${contractName}__${type}`;
+//       },
+//     }),
+//   ],
+// });
