f8797248a88fdf6cd4bb0c9d0e48f77a64c6914d
shan <shan@57blocks.com>
2024-06-11 17:54:00 +0800

    test

--- packages/react-sdk/.eslintrc.cjs
+++ packages/react-sdk/.eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   parserOptions: {
     project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
+    EXPERIMENTAL_useProjectService: true,
   },
   extends: ["@story-protocol/eslint-config"],
 };
